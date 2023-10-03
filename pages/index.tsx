c4c2732ccfb084371a33b03cb05c4f92a8f38d7b
xylaron <aldo6701@gmail.com>
2023-10-03 13:24:48 +0800

    added page title and desc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,18 +13,24 @@ export default function Home() {
     ssr: false,
   });
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-center gap-8 ${inter.className}`}
-    >
-      {/* <h1 className="text-4xl font-bold">Azure Map Testing</h1> */}
-      <div className="min-w-full flex flex-col gap-4">
-        {/* <h2 className="text-2xl font-bold">Map with Indoor Map:</h2> */}
-        <DynamicIndoorMap />
-      </div>
-      {/* <div className="min-w-full flex flex-col gap-4">
+    <>
+      <Head>
+        <title>Azure Maps Demo</title>
+        <meta name="description" content="Testing Azure Maps" />
+      </Head>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-center gap-8 ${inter.className}`}
+      >
+        {/* <h1 className="text-4xl font-bold">Azure Map Testing</h1> */}
+        <div className="min-w-full flex flex-col gap-4">
+          {/* <h2 className="text-2xl font-bold">Map with Indoor Map:</h2> */}
+          <DynamicIndoorMap />
+        </div>
+        {/* <div className="min-w-full flex flex-col gap-4">
         <h2 className="text-2xl font-bold">Default Map:</h2>
         <DynamicMap />
       </div> */}
-    </main>
+      </main>
+    </>
   );
 }
